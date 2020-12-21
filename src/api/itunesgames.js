ccb27d4ccaf978e3148ebac283702743ce4c5ed9
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2020-12-21 11:14:49 -0500

    add cache for 60 seconds

--- src/api/itunesgames.js
+++ src/api/itunesgames.js
@@ -1,41 +1,44 @@
 const express = require('express');
 const axios = require('axios');
+const rateLimit = require('express-rate-limit');
+const slowDown = require('express-slow-down');
+
+// const limiter = rateLimit({
+//    windowMs: 30 * 1000,
+//    max: 10,
+//  });
+ 
+//  const speedLimiter = slowDown({
+//    windowMs: 30 * 1000,
+//    delayAfter: 1,
+//    delayMs: 500
+//  });
+
 const router = express.Router();
 
 const BASE_URL = 'https://rss.itunes.apple.com/api/v1/us/ios-apps/new-games-we-love/all/25/explicit.json';
+// const BASE_URL = 'https://itunes.apple.com/search?term=jim+jones&country=ca';
 
-// router.get('/', (req, res, next) => {
-//   console.log(`API is running`);
-// }, 
-// async(req, res, next) => {
-//     try {
-//       const {data} = await axios.get(BASE_URL);
-//       console.log(`Data ${data}`);
-//       return res.json(data);
-//     } catch(err) {
-//       return next(err);
-//     } 
-// });
+let cacheData;
+let cacheTime;
 
-router.get('/test', async(req, res, next) => {
-       try {
-        const {data} = await axios.get(BASE_URL);
-        console.log(`Data ${data}`);
-        return res.json(data);
-      } catch(err) {
-        return next(err);
-      } 
+router.get('/games', async(req, res, next) => {
+    //Implement Authorization
+   if (cacheTime && cacheTime > Date.now() - 60 * 1000) {
+      console.log(`Returning cached data`);
+      return res.json(cacheData)
+   }
+   try {
+      const {data} = await axios.get(BASE_URL);
+      cacheData = data;
+      cacheTime = Date.now();
+      data.cacheTime = cacheTime;
+      return res.json(data);
+   } catch(err) {
+      return next(err);
+   }     
+
 });
-
-// router.get('/', async(req, res, next) => {
-//     try {
-//         const {data} = await axios.get(BASE_URL);
-//         console.log(`Data ${data}`);
-//         return res.json(data);
-//       } catch(err) {
-//         return next(err);
-//       } 
-// })
 
 module.exports = router;
 
